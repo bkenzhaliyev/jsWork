66ed2497295d0928ee5990d194bd11dc952c78fc
Bagdat <bkenzhaliyev@gmail.com>
2024-11-10 19:38:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var number = 5;
+var string = "Hello";
+var boolean = true;
+// var sym = Symbol{};
+null;
+undefined;
+var obj = {};
+
+let somesthing;
+
+//console.log(somesing);
+
+let persone = {
+    name: "Jone",
+    age: 25,
+    isMarried: false
+}
+
+console.log(persone["name"]);
+
+let arr = ['image1.jpg', 'orange.bmp', 'apple.brp'];
+
+console.log(arr[2]);
+
+// alert("Hello world!");
+
+// confirm("Are you here?");
+
+// let answer = confirm("Are you here?");
+// let answer = prompt("Есть ли Вам 18 лет?", "Да")
+// console.log(typeof(arr));
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    console.log(incr++);
+    console.log(decr--);
+
+    console.log(5%2);
+
+    console.log("2" === 2);
+
+    let isChecked = false;
+    let isClose = false;
+
+    console.log(isChecked || !isClose);
